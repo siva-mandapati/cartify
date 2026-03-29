8cf8428be4a7b8cc3eff2b0246ccd7a482ba3124
Siva Mandapati <svsk798@gmail.com>
2026-03-29 13:17:08 +0530

    fix frontend path

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,10 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 const __dirname = path.resolve();
 
-app.use("/api/orders", orderRouter);
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
